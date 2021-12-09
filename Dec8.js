8e54a849c320cb67ccee008277f9164ff6906c69
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2021-12-08 19:51:00 -0600

    Add files via upload

---
+++ Dec8.js
@@ -0,0 +1,21 @@
+let ageJazmine = '11'
+let ageYasmine = '10'
+
+
+const firstName = 'Jazmine';
+const lastName = 'Abdilahi';
+console.log(firstName + '' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJazmine > ageYasmine); // >, <, >=, <=,
+console.log(ageYasmine >= 10);
+
+const isFullAge = ageYasmine >= 10;
